d0facd76fc6c3bb0b59bd8762169af17012bd42c
Mohammed <durveshm@aston.ac.uk>
2019-01-04 14:35:41 +0000

    Removed older versions

--- Version 7/JS Testing Website/admin/README.md
+++
@@ -1,2 +0,0 @@
-# jasmine-quickstart
-This is a a Jasmine quick start for those looking to get familiar. I made this for people following my TLDR Jasmine tutorial on YouTube.
--- Version 8/JS Testing Website/test.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Write Your Test Group Descriptions Here', () => {
-    it('Write Your Test Expectation Here', () => {
-
-        expect(true).toBe(true);
-    })
-});
